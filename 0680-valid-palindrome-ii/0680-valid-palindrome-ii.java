40208a4454afdf572522504bc32093e68864ca82
Shivangi Roy <155224220+Shivangiroy20@users.noreply.github.com>
2025-06-03 14:32:20 +0530

    Time: 4 ms (99.31%), Space: 45.3 MB (90.89%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() -1;
+
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return validPalindrome(s, left + 1, right)||
+                validPalindrome(s, left, right -1);
+            }
+            left++;
+            right--;
+
+        }
+        return true;
+    }
+     boolean validPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
